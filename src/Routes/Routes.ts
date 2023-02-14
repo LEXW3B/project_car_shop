77af5566c27be3f27313a08d2e34ecac54c3a863
Alexandre <alexmanson12345@gmail.com>
2023-02-14 19:32:25 -0300

    feat: criada rota para todos os carros e pra carros selecionados

--- src/Routes/Routes.ts
+++ src/Routes/Routes.ts
@@ -4,5 +4,7 @@ import CarsController from '../Controllers/Cars.Controller';
 const router = Router();
 
 router.post('/', (req, res, next) => new CarsController(req, res, next).create());
+router.get('/', (req, res, next) => new CarsController(req, res, next).findAllCars());
+router.get('/:id', (req, res, next) => new CarsController(req, res, next).findById());
 
 export default router;

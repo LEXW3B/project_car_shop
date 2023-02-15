745a0b0934815d6c98c694c666a481393a51be77
Alexandre <alexmanson12345@gmail.com>
2023-02-14 23:08:39 -0300

    feat: criando estrutura de test da service:rocket:

---
+++ tests/unit/Services/Car.Service.test.ts
@@ -0,0 +1,15 @@
+// import { Model } from 'mongoose';
+import sinon from 'sinon';
+// import CarsService from '../../../src/Services/Cars.Service';
+
+describe('Testes da camada service', function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+
+  it('/GET - Buscar todos os carros', async function () {});
+
+  it('/GET - Buscar carros pelo id', async function () {});
+
+  it('/GET - Não é possível listar um carro que não existe', async function () {});
+});

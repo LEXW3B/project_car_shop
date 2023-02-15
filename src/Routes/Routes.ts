d03a854405b5b2be01095e26ae65dbd5fbec1998
Alexandre <alexmanson12345@gmail.com>
2023-02-15 18:22:07 -0300

    feat: criada rota put: car/id

--- src/Routes/Routes.ts
+++ src/Routes/Routes.ts
@@ -10,7 +10,10 @@ router.post('/', (req, res, next) => new CarsController(req, res, next).create()
 router.get('/', valid.listAllCars, (req, res, next) =>
   new CarsController(req, res, next).findAllCars());
 
-router.get('/:id', valid.tokenLength, valid.ifCarExist, (req, res, next) => // 
+router.get('/:id', valid.tokenLength, valid.ifCarExist, (req, res, next) =>
   new CarsController(req, res, next).findById());
+
+router.put('/:id', valid.tokenLength, valid.ifCarExist, (req, res, next) =>
+  new CarsController(req, res, next).update());
 
 export default router;

d71b5925d2419dbcc1894f142699d98ab1677bed
Alexandre <alexmanson12345@gmail.com>
2023-02-16 12:21:07 -0300

    feat: criado rota get/motorcycles

--- src/Routes/Motorcycles.route.ts
+++ src/Routes/Motorcycles.route.ts
@@ -6,4 +6,7 @@ const router = Router();
 router.post('/', (req, res, next) =>
   new MotocycleController(req, res, next).create());
 
+router.get('/', (req, res, next) =>
+  new MotocycleController(req, res, next).findAllCars());
+
 export default router;
